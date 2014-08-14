9604305c5b05e4607280c24798b2c1da324ba8c8
Christopher Cliff <chris@percolate.com>
2014-08-13 21:44:38 -0500

    Started OO style.

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,22 @@
 require('d3/d3.js')
 
-exports.map = require('./map').start
+var manifest = require('../data/routes/manifest.json')
+var Map = require('./map')
+var template = require('../templates/map.hbs')
+
+var ROUTE_URL = manifest.routes[10]
+var MAP_HTML = template()
+var ACCESS_TOKEN = 'pk.eyJ1IjoiY2hyaXN0b3BoZXJjbGlmZiIsImEiOiJqYzBuVG1jIn0.JMJhmtPBlRKnSn1m1aod0Q'
+
+L.mapbox.accessToken = ACCESS_TOKEN
+
+exports.map = initMap
+
+function initMap() {
+    var map
+    document.body.innerHTML = MAP_HTML
+    map = Map.create({
+        el: 'map'
+    })
+    d3.json(ROUTE_URL, map.addRoute.bind(map))
+}
--- src/js/map.js
+++ src/js/map.js
@@ -1,29 +1,11 @@
 var _ = require('underscore')
-var manifest = require('../data/routes/manifest.json')
-var template = require('../templates/map.hbs')
 
-var ROUTE_URL = manifest.routes[10]
-//var ROUTE_URL = '/data/us-states.json'
-var TEMPLATE = template()
 var LAYER_ID = 'christophercliff.j3973813'
-var ACCESS_TOKEN = 'pk.eyJ1IjoiY2hyaXN0b3BoZXJjbGlmZiIsImEiOiJqYzBuVG1jIn0.JMJhmtPBlRKnSn1m1aod0Q'
 
-L.mapbox.accessToken = ACCESS_TOKEN
+module.exports = Map
 
-exports.start = start
-
-function start() {
-    document.body.innerHTML = TEMPLATE
-    d3.json(ROUTE_URL, ready)
-}
-
-function ready(data) {
-    var route = _.first(data.features)
-    var featureLayer = L.mapbox.featureLayer(route)
-    var map = L.mapbox.map('map')
-    var container = d3.select(map.getPanes().overlayPane).append('svg')
-    var group = container.append('g').attr('class', 'leaflet-zoom-hide')
-    var group2 = container.append('g').attr('class', 'leaflet-zoom-hide')
+function Map(options) {
+    var map = L.mapbox.map(options.el)
     var transform = d3.geo.transform({
         point: function (x, y) {
             var point = map.latLngToLayerPoint(new L.LatLng(y, x))
@@ -31,49 +13,75 @@ function ready(data) {
         }
     })
     var path = d3.geo.path().projection(transform)
-    var feature = group.selectAll('path')
-        .data([route])
-        .enter()
-        .append('path')
-    var feature2 = group2.selectAll('path')
-        .data([route])
-        .enter()
-        .append('path')
+    var container = d3.select(map.getPanes().overlayPane).append('svg')
 
     map
-        .on('viewreset', reset)
+        .on('viewreset', this.resetRoute.bind(this))
         .addLayer(L.mapbox.tileLayer(LAYER_ID))
-        .fitBounds(featureLayer.getBounds())
 
-    reset()
+    this.map = map
+    this.transform = transform
+    this.path = path
+    this.container = container
+}
 
-    function reset() {
-        var bounds = path.bounds(data)
+_.extend(Map.prototype, {
+
+    addRoute: function (data) {
+        this.properties = data.properties
+        this.route = _.first(data.features)
+        this.group = this.container.append('g').attr('class', 'leaflet-zoom-hide')
+        this.group2 = this.container.append('g').attr('class', 'leaflet-zoom-hide')
+        this.feature = this.group.selectAll('path')
+            .data([this.route])
+            .enter()
+            .append('path')
+        this.feature2 = this.group2.selectAll('path')
+            .data([this.route])
+            .enter()
+            .append('path')
+
+        this.map.fitBounds(L.mapbox.featureLayer(this.route).getBounds())
+    },
+
+    resetRoute: function () {
+        var bounds = this.path.bounds(this.route)
         var x1 = bounds[0][0]
         var y1 = bounds[0][1]
         var x2 = bounds[1][0]
         var y2 = bounds[1][1]
 
-        container
+        this.container
             .attr('width', x2 - x1)
             .attr('height', y2 - y1)
             .style('left', x1 + 'px')
             .style('top', y1 + 'px')
 
-        group
-            .attr('transform', 'translate(' + -x1 + ', ' + -y1 + ')')
-        group2
+        this.group
             .attr('transform', 'translate(' + -x1 + ', ' + -y1 + ')')
 
-        feature
-            .attr('d', path)
-            .attr('stroke-linecap', 'round')
-            .style('stroke', data.properties.color)
+        this.group2
+            .attr('transform', 'translate(' + -x1 + ', ' + -y1 + ')')
 
-        feature2
-            .attr('d', path)
+        this.feature
+            .attr('d', this.path)
             .attr('stroke-linecap', 'round')
-            .style('stroke', 'white')
-            .style('stroke-width', '3px')
+            .style('stroke', 'rgba(0, 0, 0, 0.50)')
+            .style('stroke-width', '12px')
+
+        this.feature2
+            .attr('d', this.path)
+            .attr('stroke-linecap', 'round')
+            .style('stroke', this.properties.color)
+            .style('stroke-width', '4px')
     }
-}
+
+})
+
+_.extend(Map, {
+
+    create: function (options) {
+        return new Map(options)
+    }
+
+})
